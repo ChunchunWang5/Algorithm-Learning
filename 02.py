d9e075f51938711984a0678480ae70f273954793
ChunchunWang5 <202221110310@std.uestc.edu.cn>
2024-04-30 04:06:11 +0800

    '20240430-uf'

--- 02.py
+++ 02.py
@@ -1,1 +1,13 @@
 # https://leetcode.cn/problems/is-subsequence/
+# 392. 判断子序列
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        n, m = len(s), len(t)
+        if n>m: return False
+        l, r = 0, 0
+        while l<n and r<m:
+            if s[l] == t[r]:
+                l += 1
+            r += 1
+        return l == n
